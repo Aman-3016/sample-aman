3cde06797d3b04ad0e90c32fe674309435979b57
aman_3016 <aman.waviz@gmail.com>
2018-05-03 17:56:40 +0530

    first project

---
+++ src/main/java/com/waviz/oops/sampleaman/SampleAmanApplication.java
@@ -0,0 +1,12 @@
+package com.waviz.oops.sampleaman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAmanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/waviz/oops/sampleaman/SampleAmanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.waviz.oops.sampleaman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleAmanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
